b4fae5a036bfa03fbaff632b88581ed1d1e9ef2d
misamisa-create <missmis0329@gmail.com>
2021-07-25 13:02:57 +0000

    [Update]課題２

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,10 +1,5 @@
 class BooksController < ApplicationController
 
-  def show
-    @book = Book.find(params[:id])
-    @users = User.all
-  end
-
   def index
     @books = Book.all
     @book = Book.new
@@ -14,33 +9,39 @@ class BooksController < ApplicationController
     @book = Book.new(book_params)
     @book.user_id = current_user.id
     if @book.save
-      redirect_to book_path(@book), notice: "You have created book successfully."
+      redirect_to book_path(@book.id), notice: "You have created book successfully."
     else
       @books = Book.all
       render 'index'
     end
   end
 
+  def show
+    @show_book = Book.find(params[:id])
+    @book = Book.new
+    @users = User.all
+  end
+
   def edit
     @book = Book.find(params[:id])
   end
 
-
+  def destroy
+    @book = Book.find(params[:id])
+    @book.destroy
+    redirect_to books_path
+  end
 
   def update
-    @book = Book.find(params[:id])
-    if @book.update(book_params)
-      redirect_to book_path(@book), notice: "You have updated book successfully."
+    @show_book = Book.find(params[:id])
+    if @show_book.update(book_params)
+      redirect_to book_path(@show_book), notice: "You have updated book successfully."
     else
       render "edit"
     end
   end
 
-  def delete
-    @book = Book.find(params[:id])
-    @book.destoy
-    redirect_to books_path
-  end
+
 
   private
 

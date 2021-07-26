c2366ce97bd4129969f41388a96f8644a96bc8d7
misamisa-create <missmis0329@gmail.com>
2021-07-26 08:03:28 +0000

    [Update]残りbootstrap

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -20,6 +20,7 @@ class BooksController < ApplicationController
     @show_book = Book.find(params[:id])
     @book = Book.new
     @users = User.all
+    @book_comment = BookComment.new
   end
 
   def edit

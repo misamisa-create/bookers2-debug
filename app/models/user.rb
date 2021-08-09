6f36479de07c2217d09a1ea3258031512ef5deaf
misamisa-create <missmis0329@gmail.com>
2021-08-09 04:41:36 +0000

    [Update]8aやり直し

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,9 +9,14 @@ class User < ApplicationRecord
   has_many :book_comments,dependent: :destroy
   attachment :profile_image, destroy: false
 
-  has_many :relationships,class_name:'Relationship',foreign_key:'follower_id'
+
+  #relationshipsテーブルのfollowerカラムのどれかのidを指定する
+  # User.find(4).relationshipsだとすると、followerカラムにあるid=4の方がいるレコードが参照される
+  has_many :relationships,class_name:'Relationship',foreign_key:'follower_id',dependent: :destroy
+  # 一行目に指定したfollowerがフォローしている人を参照
+  # →この二行でfollower→followed間の関係を記述している
   has_many :followings,through: :relationships,source: :followed
-  has_many :reverse_of_relationships,class_name:'Relationship',foreign_key:'followed_id'
+  has_many :reverse_of_relationships,class_name:'Relationship',foreign_key:'followed_id',dependent: :destroy
   has_many :followers,through: :reverse_of_relationships,source: :follower
 
   def follow(other_user)
